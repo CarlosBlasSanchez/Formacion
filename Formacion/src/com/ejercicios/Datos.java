7036923827833e2a3a8604cbb1aa1a6b5d31bcb3
p-cblas <p-cblas@P-CBLAS.neoris.cxnetworks.net>
2023-03-27 18:06:43 +0200

    /Formacion/src/module-info.javaV

---
+++ Formacion/src/com/ejercicios/Datos.java
@@ -0,0 +1,44 @@
+package com.ejercicios;
+
+public class Datos {
+	
+
+		private int dia = 10;
+		private String nDni = "57.433.222";
+		private double precio;
+		private boolean ACTIVO=true;
+		public Datos(int dia, String nDni, double precio, boolean ACTIVO) {
+			this.dia = dia;
+			this.nDni = nDni;
+			this.precio = 20.50;
+			ACTIVO = ACTIVO;
+		}
+		public int getDia() {
+			return dia;
+		}
+		public void setDia(int dia) {
+			this.dia = dia;
+		}
+		public String getnDni() {
+			return nDni;
+		}
+		public void setnDni(String nDni) {
+			this.nDni = nDni;
+		}
+		public double getPrecio() {
+			return precio;
+		}
+		public void setPrecio(double precio) {
+			this.precio = precio;
+		}
+		public boolean isACTIVO() {
+			return ACTIVO;
+		}
+		public void setACTIVO(boolean ACTIVO) {
+			ACTIVO = ACTIVO;
+		}
+		
+		
+	
+
+}
