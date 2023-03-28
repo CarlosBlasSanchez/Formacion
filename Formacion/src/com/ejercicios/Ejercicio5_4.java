32213e1a51d70ab656a53d25d49423cf6315aaea
p-cblas <p-cblas@P-CBLAS.neoris.cxnetworks.net>
2023-03-28 16:04:19 +0200

    Continuación ejer5_4

--- Formacion/src/com/ejercicios/Ejercicio5_4.java
+++ Formacion/src/com/ejercicios/Ejercicio5_4.java
@@ -3,7 +3,7 @@ package com.ejercicios;
 public class Ejercicio5_4 {
 
 	public static void main(String[] args) {
-		int num=20;
+		int num=16;
 		int contDecenas=0;
 		int contUnidades=0;
 		while(num!=0) {
@@ -25,10 +25,12 @@ public class Ejercicio5_4 {
 		String arrayUnidades[]= {"", "uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve"};
 		String arrayAcabados0[]= {"cero","diez","veinte","treinta","cuarenta","cincuenta","sesenta",
 				"setenta", "ochenta", "noventa"};
-		String arrayDel11Al15[]= {"once","doce","trece","catorce"};
+		String arrayDel11Al15[]= {"once","doce","trece","catorce","quince"};
 		if (contUnidades==0)
 			System.out.println(arrayAcabados0[contDecenas]);
-		else 
+		else if (contDecenas==1 && contUnidades>=1 && contUnidades<=5)
+				System.out.println(arrayDel11Al15[contUnidades-1]);
+			else
 			System.out.println(arrayDecenas[contDecenas]+arrayUnidades[contUnidades]);
 
 		
